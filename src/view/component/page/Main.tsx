8d7947651f94455231ead804940ab68c1c2961b9
DAECHUNG YOO <dcyoo777@gmail.com>
2024-11-13 16:51:10 +0900

    FEAT: 컨트롤러 위치 번경

--- src/view/component/page/Main.tsx
+++ src/view/component/page/Main.tsx
@@ -1,14 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import './Main.scss';
 import {useDispatch, useSelector} from "react-redux";
-import {createSection, selectSectionIds} from "../../../redux/survey";
+import {createQuestion, createSection, selectMode, selectSectionIds} from "../../../redux/survey";
 import Section from "../section/Section";
+import {SURVEY_MODE} from "../../../redux/type";
+import {MdOutlineAddCircleOutline} from "react-icons/md";
+import {CiGrid2H} from "react-icons/ci";
 
 function Main() {
 
     const dispatch = useDispatch();
 
+    const mode = useSelector(selectMode);
     const sectionIds = useSelector(selectSectionIds);
+
+    const onCreateSection = useCallback(() => {
+        // @ts-ignore
+        dispatch(createSection());
+    }, [dispatch]);
+
+    const onCreateQuestion = useCallback(() => {
+    //     dispatch(createQuestion({sectionId}));
+    // }, [dispatch, sectionId]);
+    }, [dispatch]);
 
     useEffect(() => {
         if (sectionIds.length === 0) {
@@ -19,6 +33,16 @@ function Main() {
 
     return (
         <main>
+            {mode === SURVEY_MODE.EDIT && <div className={"controller"}>
+                <div className={"controller-body"}>
+                    <button className={"controller-button"} onClick={onCreateQuestion}>
+                        <MdOutlineAddCircleOutline />
+                    </button>
+                    <button className={"controller-button"} onClick={onCreateSection}>
+                        <CiGrid2H />
+                    </button>
+                </div>
+            </div>}
             {sectionIds.map((sectionId) => <Section key={`section_${sectionId}`} sectionId={sectionId}/>)}
         </main>
     );
--- src/view/component/section/Section.tsx
+++ src/view/component/section/Section.tsx
@@ -1,13 +1,11 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import SectionHeader from "./SectionHeader";
-import {useDispatch, useSelector} from "react-redux";
-import {createQuestion, selectMode, selectSectionById} from "../../../redux/survey";
+import {useSelector} from "react-redux";
+import {selectSectionById} from "../../../redux/survey";
 import {RootState} from "../../../redux";
 import {EntityId} from "@reduxjs/toolkit";
 import Question from "../question/Question";
-import { MdOutlineAddCircleOutline } from "react-icons/md";
-import './Section.scss'
-import {SURVEY_MODE} from "../../../redux/type";
+import './Section.scss';
 
 type SectionProps = {
     sectionId: EntityId;
@@ -15,23 +13,11 @@ type SectionProps = {
 
 function Section({sectionId}: SectionProps) {
 
-    const dispatch = useDispatch();
-
-    const mode = useSelector(selectMode);
     const section = useSelector((state: RootState) => selectSectionById(state, sectionId));
-
-    const onCreateQuestion = useCallback(() => {
-        dispatch(createQuestion({sectionId}));
-    }, [dispatch, sectionId]);
 
     return (
         <div className={"section"}>
             <SectionHeader section={section} />
-            {mode === SURVEY_MODE.EDIT && <div className={"section-controller"}>
-                <button className={"section-controller-add"} onClick={onCreateQuestion}>
-                    <MdOutlineAddCircleOutline />
-                </button>
-            </div>}
             {section.questions.map((question) =>
                 <Question key={`section-${sectionId}_question-${question.id}`}
                           sectionId={sectionId}
